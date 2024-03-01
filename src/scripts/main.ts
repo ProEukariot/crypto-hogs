3bd90af7dbb2cfd1230127d1f51c8ff83f4e7465
GeorGius11 <george.rybko@gmail.com>
2024-03-01 22:50:45 +0200

    app auto-update

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,15 +1,19 @@
+import * as path from "path";
 const { app, BrowserWindow } = require("electron");
+require("electron-reload")(path.resolve(__dirname, "../"), {
+  electron: require(path.resolve(__dirname, "../node_modules/electron")),
+});
 
 const createWindow = () => {
-	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		autoHideMenuBar: true,
-	});
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+  });
 
-	win.loadFile("index.html");
+  win.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
-	createWindow();
+  createWindow();
 });
